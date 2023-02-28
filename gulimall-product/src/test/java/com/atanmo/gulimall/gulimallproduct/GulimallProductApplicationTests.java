76a5695dcafabedb6d89634bcea368fbee4ef3f7
Atanmo <877705308@qq.com>
2023-02-28 16:10:40 +0800

    项目初始化结构

---
+++ gulimall-product/src/test/java/com/atanmo/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atanmo.gulimall.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
